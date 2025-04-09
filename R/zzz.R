dc707117beffbbad45a2d9d2d80f0940f771d49a
Devin Johnson <devin.johnson@noaa.gov>
2025-04-09 12:56:08 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,5 @@
                         "  A trimmed version of the dsmextra package.\n",
                         "-----------------------------------------------\n",
                         "* Please see:\nBouchet PJ, Miller DL, Roberts JJ, Mannocci L, Harris CM, Thomas L (2020). dsmextra: Extrapolation assessment tools for density surface models. Methods in Ecology and Evolution, 11(11): 1464-1469. DOI: 10.1111/2041-210X.13469\n",
-                        "\n* Quick start guide:\nA vignette is available at:\nhttps://densitymodelling.github.io/dsmextra/articles/dsmextra.html\n",
                         "\n* Further information:\ndsmextra is an output of the DenMod project. For more details, please visit:\nhttps://synergy.st-andrews.ac.uk/denmod/")
 }
